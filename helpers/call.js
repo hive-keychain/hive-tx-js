e094a8b892088a28040946e2e2139deef7ec5ebf
Cedric Gui <cedric.guillas78@gmail.com>
2023-02-07 06:23:46 +0800

    updated reject in call

--- helpers/call.js
+++ helpers/call.js
@@ -11,28 +11,33 @@ const call = async (method, params = [], timeout = 10, overrideRpc = undefined) 
     let resolved = false
 
   return new Promise((resolve, reject) => {
-    fetch(overrideRpc ? overrideRpc : config.node, {
-      method: 'POST',
-      body: JSON.stringify({
-        jsonrpc: '2.0',
-        method,
-        params,
-        id: 1
-      }),
-      headers: { 'Content-Type': 'application/json' }
-    })
-    .then(res => {
-      if (res && res.status === 200) {
-        resolved = true
-        resolve(res.json())
-      }
-    })
+    try {
+      fetch(overrideRpc ? overrideRpc : config.node, {
+        method: 'POST',
+        body: JSON.stringify({
+          jsonrpc: '2.0',
+          method,
+          params,
+          id: 1
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      })
+      .then(res => {
+        if (res && res.status === 200) {
+          resolved = true
+          resolve(res.json())
+        }
+      })
 
-    setTimeout(() => {
-      if (!resolved) {
-        reject(new Error('Network timeout.'))
-      }
-    }, timeout * 1000)
+      setTimeout(() => {
+        if (!resolved) {
+          reject(new Error('Network timeout.'))
+        }
+      }, timeout * 1000)
+    } catch(err){
+      resolved = true;
+      reject(err);
+    }
   })
 }
 
