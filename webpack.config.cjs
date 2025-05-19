10d90b3bd7ca11e7d52b0627ea22d7c976f8b6b1
stoodkev <quentin.correa@gmail.com>
2025-05-19 15:36:51 +0800

    v4.3

---
+++ webpack.config.cjs
@@ -0,0 +1,31 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'hive-tx.min.js',
+    path: path.resolve(__dirname, 'dist'),
+    globalObject: 'this',
+    library: {
+      name: 'hiveTx',
+      type: 'umd'
+    }
+  },
+  devtool: 'source-map',
+  resolve: {
+    fallback: {
+      'process/browser': require.resolve('process/browser'),
+      'buffer': require.resolve('buffer'),
+      'stream': require.resolve('stream-browserify'),
+    }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser'
+    }),
+    new webpack.NormalModuleReplacementPlugin(/node:/, (resource) => {
+      resource.request = resource.request.replace(/^node:/, '')
+    })
+  ]
+}
